4bdc74ebf0afcdb81de6082d94785d101ac0b427
DEVigodE <moreira_igor9@hotmail.com>
2025-09-21 10:50:18 -0300

    Refactor Objetos controller and view: update controller name, add onClickSetText method, and enhance view structure with additional UI elements.

--- webapp/controller/Objetos.controller.js
+++ webapp/controller/Objetos.controller.js
@@ -4,9 +4,15 @@ sap.ui.define([
 function (Controller) {
     "use strict";
 
-    return Controller.extend("br.com.nel.freestyle.ibm.report.products.controller.Objetos", {
+    return Controller.extend("br.com.nel.freestyle.ibm.report.products.controller.Objestos", {
         onInit: function () {
 
+        },
+        onClickSetText: function () {
+            var oTitle = this.getView().byId("title1");
+            var sText = oTitle.getText();
+            oTitle.setText("Texto Alterado");
+            alert("O texto do header é: " + sText);
         }
     });
 });
